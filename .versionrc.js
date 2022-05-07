a662eca52a35d4d74f58d4ed42425c516b206c1c
zhangmin.jammy <zhangmin.jammy@bytedance.com>
2022-05-07 20:39:48 +0800

    feat: init

--- .versionrc.js
+++ .versionrc.js
@@ -1,27 +1,43 @@
 module.exports = {
-  "types": [
-    {"type": "feat", "section": "新功能", hidden: false},
-    {"type": "fix", "section": "修复", hidden: false},
-    {"type": "chore", "section": "配置", hidden: false},
-    {"type": "docs", "section": "文档", "hidden": true},
-    {"type": "style", "section": "样式", "hidden": true},
-    {"type": "refactor", "section": "重构", "hidden": true},
-    {"type": "perf", "section": "性能", "hidden": true},
-    {"type": "test", "section": "测试", "hidden": true},
+  types: [
+    { type: 'feat',
+      section: '新功能',
+      hidden: false },
+    { type: 'fix',
+      section: '修复',
+      hidden: false },
+    { type: 'chore',
+      section: '配置',
+      hidden: false },
+    { type: 'docs',
+      section: '文档',
+      hidden: true },
+    { type: 'style',
+      section: '样式',
+      hidden: true },
+    { type: 'refactor',
+      section: '重构',
+      hidden: true },
+    { type: 'perf',
+      section: '性能',
+      hidden: true },
+    { type: 'test',
+      section: '测试',
+      hidden: true },
     {
       type: 'revert',
       section: '回退',
-      hidden: true
+      hidden: true,
     },
     {
       type: 'build',
       section: '构建',
-      hidden: true
+      hidden: true,
     },
     {
       type: 'ci',
       section: '部署',
-      hidden: true
-    }
-  ]
-}
+      hidden: true,
+    },
+  ],
+};
